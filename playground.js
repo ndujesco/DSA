2b118d8aaca0c6d65ccc61bb5ac85f395e8160a1
ugo <ndujesco@users.noreply.github.com>
2023-08-18 22:58:46 +0100

    update

--- playground.js
+++ playground.js
@@ -21,6 +21,18 @@
 //   ])
 // );
 
-console.log(0.1 + 0.2 === 0.30000000000000004);
+// console.log(0.1 + 0.2 === 0.30000000000000004);
 // console.log(("3" + 7));
 // console.log("3" - 7);
+let unique = [...new Set(["penny", "and", "dime"])].sort((a, b) =>
+  a < b ? -1 : 1
+); // ['username1', 'username2']
+
+// console.log(...new Set([["penny", "and", "dime"]]));
+let updatedRoomName = `${unique[0]}--with--${unique[1]}`; //
+// console.log(updatedRoomName);
+// console.log(Array.from('penny s  s'));
+
+// console.log("penny-with-".match(/-with-/));
+
+console.log([1, 2, 3, 4, 5, 6, 7, 8].slice(0, 5));
